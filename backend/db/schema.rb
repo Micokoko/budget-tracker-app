c51825b504d744421ae8894ce23a7db96abbc39e
Micokoko <mico.balina@gmail.com>
2024-10-28 13:43:40 +0800

    converted to postgreSQL

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_10_28_051400) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.date "date"
     t.string "entry_type"
     t.string "description"
