70d5fc79de2fc620cc93fe23acbcef1df03d83e3
Micokoko <mico.balina@gmail.com>
2024-10-28 11:48:04 +0800

    inital commit. Set up of Rails, React and Tailwind structure

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Look for classes in your React component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
