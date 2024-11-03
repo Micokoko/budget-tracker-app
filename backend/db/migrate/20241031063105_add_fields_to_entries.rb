ecae10b443239dcc2bf463c1da438e808d0a6424
Micokoko <mico.balina@gmail.com>
2024-11-03 14:36:28 +0800

    fixed migrations

--- backend/db/migrate/20241031063105_add_fields_to_entries.rb
+++ backend/db/migrate/20241031063105_add_fields_to_entries.rb
@@ -1,8 +1,8 @@
 class AddFieldsToEntries < ActiveRecord::Migration[7.2]
   def change
-    add_column :entries, :date, :date
-    add_column :entries, :entry_type, :string
-    add_column :entries, :description, :string
-    add_column :entries, :amount, :decimal
+    add_column :entries, :date, :date unless column_exists?(:entries, :date)
+    add_column :entries, :entry_type, :string unless column_exists?(:entries, :entry_type)
+    add_column :entries, :description, :string unless column_exists?(:entries, :description)
+    add_column :entries, :amount, :decimal unless column_exists?(:entries, :amount)
   end
 end
--- backend/db/migrate/20241031063247_add_user_id_to_entries.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToEntries < ActiveRecord::Migration[7.2]
-  def change
-    add_reference :entries, :user, null: false, foreign_key: true
-  end
-end
